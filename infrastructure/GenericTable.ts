e36b93bd609625b3074101910962a67aab41ba63
TonyWar11 <guerra.10510@gmail.com>
2022-01-22 11:44:21 +0000

    lambda integration

---
+++ infrastructure/GenericTable.ts
@@ -0,0 +1,35 @@
+import { Stack } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { textChangeRangeIsUnchanged } from 'typescript';
+
+export class GenericTable {
+
+    private name: string;
+    private primaryKey: string;
+    //reference stack
+    private stack: Stack;
+    private table: Table;
+
+    public constructor(name: string, primaryKey: string, stack: Stack) {
+        this.name = name;
+        this.primaryKey = primaryKey;
+        this.stack = stack;
+        this.initialize();
+    }
+
+    //initializer
+    private initialize(){
+        this.createTable();
+    }
+
+    private createTable(){
+        this.table = new Table(this.stack, this.name, {
+            partitionKey: {
+                name: this.primaryKey,
+                type: AttributeType.STRING
+            },
+            tableName: this.name
+        })
+    }
+
+}
