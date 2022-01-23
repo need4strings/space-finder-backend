5577267e6e92ba81c0348fdf42a228f457c83568
TonyWar11 <guerra.10510@gmail.com>
2022-01-23 10:43:08 +0000

    added lambda permition to list buckets | debugging locally

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,9 +1,18 @@
 import { v4 } from 'uuid';
+import { S3 } from 'aws-sdk';
 
+// creating s3 client
+const s3Client = new S3();
+
+//before deploying we have to give this lambda the permition to list otherwise we will get an Access Denied error
 async function handler(event: any, context: any) {
+    const buckets = await s3Client.listBuckets().promise();
+    console.log('GOT AN EVENT:');
+    console.log(event);
+
     return {
         statusCode: 200,
-        body: "Hello from Lambda!" + v4()
+        body: "Here are your buckets: " + JSON.stringify(buckets.Buckets)
     }
 }
 
